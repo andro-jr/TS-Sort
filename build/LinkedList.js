cec11477c6bee8a755cda1ea73c412da01373cae
andro-jr <prabin20panta@gmail.com>
2024-05-14 12:09:23 +0545

    additional linked list functionality

--- build/LinkedList.js
+++ build/LinkedList.js
@@ -23,5 +23,48 @@ class LinkedList {
         }
         tail.next = node;
     }
+    get length() {
+        if (!this.head)
+            return 0;
+        let length = 1;
+        let node = this.head;
+        while (node.next) {
+            length++;
+            node = node.next;
+        }
+        return length;
+    }
+    at(index) {
+        if (!this.head)
+            throw new Error('Index out of bound!');
+        let counter = 0;
+        let node = this.head;
+        while (node) {
+            if (counter === index)
+                return node;
+            counter++;
+            node = node.next;
+        }
+        throw new Error('Index out of bound');
+    }
+    compare(leftIndex) {
+        if (!this.head)
+            throw new Error('List is empty!');
+        return this.at(leftIndex).data > this.at(leftIndex + 1).data;
+    }
+    swap(leftIndex) {
+        const temp = this.at(leftIndex).data;
+        this.at(leftIndex).data = this.at(leftIndex + 1).data;
+        this.at(leftIndex + 1).data = temp;
+    }
+    print() {
+        if (!this.head)
+            throw new Error('List is empty!');
+        let node = this.head;
+        while (node) {
+            console.log(node.data);
+            node = node.next;
+        }
+    }
 }
 exports.LinkedList = LinkedList;
--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -21,4 +21,54 @@ export class LinkedList {
 
     tail.next = node;
   }
+
+  get length(): number {
+    if (!this.head) return 0;
+
+    let length = 1;
+    let node = this.head;
+    while (node.next) {
+      length++;
+      node = node.next;
+    }
+
+    return length;
+  }
+
+  at(index: number): Node {
+    if (!this.head) throw new Error('Index out of bound!');
+
+    let counter = 0;
+    let node: Node | null = this.head;
+    while (node) {
+      if (counter === index) return node;
+
+      counter++;
+      node = node.next;
+    }
+
+    throw new Error('Index out of bound');
+  }
+
+  compare(leftIndex: number): boolean {
+    if (!this.head) throw new Error('List is empty!');
+
+    return this.at(leftIndex).data > this.at(leftIndex + 1).data;
+  }
+
+  swap(leftIndex: number): void {
+    const temp = this.at(leftIndex).data;
+    this.at(leftIndex).data = this.at(leftIndex + 1).data;
+    this.at(leftIndex + 1).data = temp;
+  }
+
+  print(): void {
+    if (!this.head) throw new Error('List is empty!');
+    let node: Node | null = this.head;
+    while (node) {
+      console.log(node.data);
+
+      node = node.next;
+    }
+  }
 }
